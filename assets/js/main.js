8886a8544690d4ba3565997538053f1fffaae9c1
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-10-31 21:02:48 +0600

    scroll-top added

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,6 +38,15 @@ function scrollHeader() {
 }
 window.addEventListener("scroll", scrollHeader);
 
+// Scroll Top
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 500) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
 const headerTitle = document.getElementById("header-title");
 
 const typewriter = new Typewriter(headerTitle, {
